1dd5e1864e8c99b99729503a6c5542d79821ba2a
Luiz Henrique Lima Da Silva <luizhenriquesilva@id.uff.br>
2020-10-19 13:05:59 -0300

    amostragem_lista1_ex3

--- amostragem/exercicio1lista.R
+++ amostragem/exercicio1lista.R
@@ -2,8 +2,7 @@ library(styler)
 library(ggplot2)
 library(dplyr)
 setwd("~/Documentos/Git/aulas_R/amostragem")
-
-#1 ) #####
+# 1 ) #####
 n <- 1000
 N <- 36000
 
@@ -11,6 +10,13 @@ cadastro <- data.frame(
   n_operarios = c(451, 162, 187, 112, 49, 21, 5, 11, 2),
   n_faltas = c(0, 1, 2, 3, 4, 5, 6, 7, 8)
 )
+
+ggplot(
+  data.frame(cont = rep(cadastro$n_faltas, cadastro$n_operarios)),
+  aes(cont)
+) +
+  geom_density()
+
 
 # a seja x numero de faltas no i-essimo operario
 sum_xi <- sum(cadastro$n_operarios * cadastro$n_faltas)
@@ -28,31 +34,6 @@ var_x # estimativa para a variancia do numero de faltas
 z_alfa2 <- qnorm(0.025, lower.tail = F)
 ic <- c(x_barra - z_alfa2 * sqrt(var_x), x_barra + z_alfa2 * sqrt(var_x))
 ic # intervalo com chance de conter a verdadeira media amostral
-plot_ic
-
-ggplot(NULL, aes(c(ic[1] - 10 * var_x, ic[2] + 10 * var_x))) +
-  geom_area(
-    stat = "function",
-    fun = dnorm,
-    args = list(mean = x_barra, sd = sqrt(var_x)),
-    fill = "red",
-    xlim = c(ic[1] - 20 * var_x, ic[1])
-  ) +
-  geom_area(
-    stat = "function",
-    fun = dnorm,
-    args = list(mean = x_barra, sd = sqrt(var_x)),
-    fill = "red",
-    xlim = c(ic[2], ic[2] + 20 * var_x)
-  ) +
-  geom_area(
-    stat = "function",
-    fun = dnorm,
-    args = list(mean = x_barra, sd = sqrt(var_x)),
-    fill = "blue",
-    xlim = c(ic[1], ic[2]),
-    alpha = 0.7
-  ) # grafico do intervalo de confianca
 
 # c
 err <- (ic[2] - ic[1])
@@ -63,7 +44,7 @@ novo_n <- (z_alfa2^2 * N * s2) / (z_alfa2^2 * s2 + N * n_err^2)
 ceiling(novo_n) # novo numero de funcionarios para fazer a amostra
 
 rm(list = ls())
-#2 ) #####
+# 2 ) #####
 # a)
 set.seed(999)
 hajek <- data.frame(
@@ -74,14 +55,77 @@ hajek <- data.frame(
   )
 )
 empresas <- dplyr::arrange(hajek, aleatorio)
+
+ggplot(
+  data.frame(cont = empresas$receita),
+  aes(cont)
+) +
+  geom_density()
+
 n <- 3 # tamanho da amostra
 N <- nrow(empresas) # tamanho populacao
-amostra <- empresas[1:n,]
+amostra <- empresas[1:n, ]
 
-#b
+# b
 x_barra <- mean(amostra$receita)
 x_barra # estimativa da receita media das empresas
 
-total_x <- N*sum(amostra$receita)
-total_x # estimativa para o total de receita na populacao
-sum(empresas$receita)
+x_total <- N * x_barra
+x_total # estimativa para o total de receita na populacao
+
+# c
+sum_xi <- x_barra * n
+sum_xi2 <- sum(amostra$receita^2)
+
+s2 <- ((sum_xi2 / n) - (x_barra^2)) * (n / (n - 1))
+s2
+
+var_xbarra <- ((N - n) / N) * (s2 / n)
+var_xbarra # estimativa para a variancia do numero de faltas
+cov_xbarra <- sqrt(var_xbarra) / x_barra * 100
+cov_xbarra # estimativa do coeficiente de variacao da media pupulacional
+
+var_xtotal <- N * (N - 1) * s2 / n
+var_xtotal # estimativa para a variancia do total
+cov_xtotal <- sqrt(var_xtotal) / x_total * 100
+cov_xtotal # estimativa para o coeficiente de variacao do total pop
+
+rm(list = ls())
+# 3 ) ####
+N <- 100
+n <- 10
+amostra <- c(2, 5, 1, 4, 4, 3, 2, 5, 2, 3)
+sum_xi <- sum(amostra)
+sum_xi2 <- sum(amostra^2)
+
+ggplot(
+  data.frame(cont = amostra),
+  aes(cont)
+) +
+  geom_density()
+
+
+# a
+x_barra <- sum_xi / n
+x_barra # estimador da media populacional
+
+s2 <- (n / (n - 1)) * (1 / n * sum_xi2 - x_barra^2)
+s2
+var_xbarra <- ((N - n) / (N * n)) * s2
+var_xbarra # variancia do estimador da media no plano AASsr
+
+cov_xbarra <- sqrt(var_xbarra) / x_barra * 100
+cov_xbarra # coeficiente de variacao do estimador da media
+
+
+# b
+x_total <- N * x_barra
+x_total
+
+var_xtotal <- N * (N - n) * s2 / n
+var_xtotal # variancia do estimador do total no plano AASsr
+cov_xtotal <- sqrt(var_xtotal) / x_total * 100
+cov_xtotal # coeficiente de variacao do estimador do total
+
+
+save.image("~/Documentos/Git/aulas_R/amostragem/exercicio1list.RData")
